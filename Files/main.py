615c659abe4808fd90e0f9d4db1729734e2e63df
LeifHoffman <hoffmanl2@etown.edu>
2024-11-07 11:45:17 -0500

    Updated main to create a gradio page

--- Files/main.py
+++ Files/main.py
@@ -1,3 +1,4 @@
+import gradio as gr
 import anthropic
 
 client = anthropic.Anthropic(
@@ -5,10 +6,26 @@ client = anthropic.Anthropic(
     api_key="my_api_key",
 )
 
-message = client.messages.create(
-    model="claude-3-5-sonnet-20240620",
-    max_tokens=1000,
-    temperature=0,
-    messages=[]
-)
-print(message.content)
+def get_response(request):
+    message = client.messages.create(
+        model="claude-3-5-sonnet-20240620",
+        max_tokens=1000,
+        temperature=0,
+        system="Translate the given code into Java code.",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": request
+                    }
+                ]
+            }
+        ]
+    )
+    return message.content
+
+demo = gr.Interface(fn=get_response, inputs="text", outputs="text")
+    
+demo.launch(share=True) 
